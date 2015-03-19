2553d7c642c8fce8a4ac87b79ff9564159546d12
jeanyves-yang <jean-yves.yang@cpe.fr>
2015-03-19 15:48:09 -0400

    ENH: MaurerDistanceMap functional.

--- MaurerDistanceTransform.cxx
+++ MaurerDistanceTransform.cxx
@@ -4,46 +4,27 @@
 #include "itkSignedMaurerDistanceMapImageFilter.h"
 #include "MaurerDistanceTransformCLP.h"
 
-
-
-/* Creates the MaurerDistanceTransform filter and the output image with the input type. */
-int MaurerDistanceTransform( std::string inputFileName , std::string outputFileName )
-{
-  typedef unsigned int InputComponentType ;
-  typedef int OutputComponentType ;
-  typedef itk::Image< InputComponentType , 3 > InputImageType ;
-  typedef itk::Image< OutputComponentType , 3 > OutputImageType ;
-  typedef itk::SignedMaurerDistanceMapImageFilter < InputImageType , OutputImageType > FilterType ;
-  FilterType::Pointer MaurerDistanceMapImageFilter = FilterType::New() ;
-  InputImageType::Pointer image = InputImageType::New() ;
-  typedef itk::ImageFileReader< InputImageType > ReaderType ;
-  ReaderType::Pointer reader = ReaderType::New() ;
-  reader->SetFileName( inputFileName.c_str() ) ;
-  try
-  {
-    reader->Update() ;
-  }
-  catch(...)
-  {
-    std::cerr << "Image could not be read." << std::endl ;
-    return EXIT_FAILURE ;
-  }
-  MaurerDistanceMapImageFilter->SetInput( image ) ;
-  MaurerDistanceMapImageFilter->Update();
-  typedef itk::ImageFileWriter< OutputImageType > WriterType ;
-  WriterType::Pointer writer = WriterType::New() ;
-  writer->SetInput( MaurerDistanceMapImageFilter->GetOutput() ) ;
-  writer->SetFileName( outputFileName ) ;
-  writer->Update() ;
-  return EXIT_SUCCESS ;
-}
-
-
-
 int main(int argc, char * argv[])
 {
+    typedef itk::Image<unsigned char , 3> UnsignedCharImageType;
+    typedef itk::Image<float , 3> FloatImageType;
     PARSE_ARGS;
-    return MaurerDistanceTransform( input , output  ) ;
+    UnsignedCharImageType::Pointer image = UnsignedCharImageType::New();
+    typedef itk::ImageFileReader<UnsignedCharImageType> ReaderType;
+    ReaderType::Pointer reader = ReaderType::New();
+    reader->SetFileName(input);
+    reader->Update();
+    image = reader->GetOutput();
 
+    typedef itk::SignedMaurerDistanceMapImageFilter< UnsignedCharImageType, FloatImageType > SignedMaurerDistanceMapImageFilterType;
+    SignedMaurerDistanceMapImageFilterType::Pointer distanceMapImageFilter =
+            SignedMaurerDistanceMapImageFilterType::New();
+    distanceMapImageFilter->SetInput( image ) ;
+    distanceMapImageFilter->Update();
+    typedef itk::ImageFileWriter< FloatImageType > WriterType;
+    WriterType::Pointer writer = WriterType::New() ;
+    writer->SetInput( distanceMapImageFilter->GetOutput() ) ;
+    writer->SetFileName( output ) ;
+    writer->Update() ;
+    return EXIT_SUCCESS ;
 }
-
